f45ef8d2de7e2dc12a48ce208fc0c55809a98436
vasantha <kvasanth373@gmail.com>
2020-11-04 16:31:22 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/iamvasanth07/go-micro-services/handlers"
+)
+
+func main() {
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	http.ListenAndServe(":8080", sm)
+}
